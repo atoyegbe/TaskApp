62578fb9f93d94e9d89a38e98911ce6432280201
atoyegbe <herdeyemie@gmail.com>
2020-07-12 04:38:23 -0700

    First quarter commit

---
+++ tasks/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path, include
+
+from .views import *
+urlpatterns = [
+	path('', home, name='home'),
+	path('tasks/', viewTasks, name="tasks"),
+
+	path('task/<str:pk>', taskView, name="task"),
+	path('delete/<str:pk>', deleteTask, name="delete")
+]
---
+++ tasks/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect
+from .forms import TaskForm 
+from .models import *
+# Create your views here.
+def home(request):
+	forms = TaskForm()
+	if request.method == 'POST':
+		forms = TaskForm(request.POST)
+		if forms.is_valid():
+			forms.save()
+
+			return redirect('home')
+
+	template_name = 'index.html'
+	return render(request, template_name, context={'form': forms})
+
+
+def viewTasks(request):
+	tasks = Task.objects.all()
+	template_name='tasks.html'
+
+	return render(request, template_name, context={'tasks': tasks})
+
+def taskView(request, pk):
+	task = Task.objects.get(id=pk)
+
+	template_name = 'task.html'
+	context = {'task': task}
+	return render(request, template_name, context)
+
+def deleteTask(request, pk):
+
+	task_d = Task.objects.get(id=pk)
+
+	if request.method == 'POST':
+		task_d.delete()
+		return redirect('tasks')
+
+	template_name='delete.html'
+	context = {'task_d': task_d}
+
+	return render(request, template_name, context)
+
